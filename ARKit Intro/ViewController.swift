0a19816ba82e060cc20b5f85e8d629e6c4268bd1
Charles Martin Reed <charlesmartinreed@icloud.com>
2019-01-09 16:05:15 -0600

    Add ability to place measurement points on the map.

--- ARKit Intro/ViewController.swift
+++ ARKit Intro/ViewController.swift
@@ -12,7 +12,12 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
+    //MARK:- IBOutlets
     @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var targetView: UIView!
+    @IBOutlet weak var measurementLabel: UILabel!
+    @IBOutlet weak var startingPointButton: UIButton!
+    @IBOutlet weak var endingPointButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,11 +28,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,6 +35,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        //for now, just detecting flat surfaces like tables. The vertical option is for detecting surfaces like walls.
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -46,7 +49,54 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
+    
+    //MARK:- IBActions
+    @IBAction func startingPointButtonTapped(_ sender: UIButton) {
+        //add a block to the 3D world when the user taps
+        let userTouch = sceneView.center
+        
+        //where is the touch in the AR scene
+        let testResults = sceneView.hitTest(userTouch, types: .featurePoint)
+        
+        if let theResult = testResults.first {
+            //create a block in AR World
+            let box = SCNBox(width: 0.005, height: 0.005, length: 0.005, chamferRadius: 0.005)
+            let material = SCNMaterial()
+            material.diffuse.contents = UIColor.green
+            box.firstMaterial = material
+            
+            let boxNode = SCNNode(geometry: box)
+            let boxX = theResult.worldTransform.columns.3.x
+            let boxY = theResult.worldTransform.columns.3.y
+            let boxZ = theResult.worldTransform.columns.3.z
+            
+            boxNode.position = SCNVector3(boxX, boxY, boxZ)
+            
+            //add the box to the world
+            sceneView.scene.rootNode.addChildNode(boxNode)
+        }
+    }
+    
+    @IBAction func endingPointButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+//        //when a surface is found, a node will be added
+//        print("added node")
+//
+//        //how big was the thing we just found? Check the anchor
+//        if let plane = anchor as? ARPlaneAnchor {
+//            print("X: \(plane.extent.x)m, Z: \(plane.extent.z)")
+//        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+//        if let plane = anchor as? ARPlaneAnchor {
+//            print("X: \(plane.extent.x)m, Z: \(plane.extent.z)")
+//        }
+    }
     // MARK: - ARSCNViewDelegate
     
 /*

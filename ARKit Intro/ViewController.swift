70a9921af8cca5a2796f6fba9baa33f3e3e3edfb
Charles Martin Reed <charlesmartinreed@icloud.com>
2019-01-09 16:50:04 -0600

    Implement measuring and reset methods.

--- ARKit Intro/ViewController.swift
+++ ARKit Intro/ViewController.swift
@@ -19,6 +19,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet weak var startingPointButton: UIButton!
     @IBOutlet weak var endingPointButton: UIButton!
     
+    //MARK:- Properties
+    var firstBox: SCNNode?
+    var secondBox: SCNNode?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,6 +31,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        
+        startingPointButton.setTitle("Set First Point", for: .normal)
+        measurementLabel.text = ""
         
     }
     
@@ -52,6 +59,32 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     //MARK:- IBActions
     @IBAction func startingPointButtonTapped(_ sender: UIButton) {
+        
+        if firstBox == nil {
+            firstBox = createBoxPoint()
+            startingPointButton.setTitle("Set Second Point", for: .normal)
+        } else if firstBox != nil {
+            if secondBox == nil {
+                secondBox = createBoxPoint()
+            }
+        }
+    }
+    
+    @IBAction func endingPointButtonTapped(_ sender: UIButton) {
+        if firstBox != nil && secondBox != nil {
+            calcDistance()
+            endingPointButton.setTitle("Reset", for: .normal)
+        }
+        
+        if sender.titleLabel?.text == "Reset" {
+            print("resetting")
+            resetBoxPoints()
+            endingPointButton.setTitle("Measure", for: .normal)
+        }
+    }
+    
+    //MARK:- Placing box objects in the ARScene
+    func createBoxPoint() -> SCNNode? {
         //add a block to the 3D world when the user taps
         let userTouch = sceneView.center
         
@@ -66,6 +99,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             box.firstMaterial = material
             
             let boxNode = SCNNode(geometry: box)
+            boxNode.name = "measurePoint"
             let boxX = theResult.worldTransform.columns.3.x
             let boxY = theResult.worldTransform.columns.3.y
             let boxZ = theResult.worldTransform.columns.3.z
@@ -74,10 +108,43 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             
             //add the box to the world
             sceneView.scene.rootNode.addChildNode(boxNode)
+            return boxNode
         }
+        return nil
     }
     
-    @IBAction func endingPointButtonTapped(_ sender: UIButton) {
+    //MARK:- Calculating the distance between two points
+    func calcDistance() {
+        guard let firstBox = firstBox else { return }
+        guard let secondBox = secondBox else { return }
+        
+        //using the pythagorean theorem to figure out our distance
+        let vector = SCNVector3Make(secondBox.position.x - firstBox.position.x, secondBox.position.y - firstBox.position.y, secondBox.position.z - firstBox.position.z)
+        
+        let distance = sqrt(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z) * 39.37 //inches
+        updateDistanceLabel(distance: String(distance))
+    }
+    
+    //MARK:- Updating the UI
+    func updateDistanceLabel(distance: String) {
+        measurementLabel.text = "\(distance) in."
+    }
+    
+    //MARK:- Resetting the scene for new measurements
+    func resetBoxPoints() {
+        for node in sceneView.scene.rootNode.childNodes {
+            if node.name == "measurePoint" {
+                node.removeFromParentNode()
+            }
+        }
+        updateDistanceLabel(distance: "")
+        //endingPointButton.setTitle("Measure", for: .normal)
+        startingPointButton.setTitle("Set First Point", for: .normal)
+        
+        //nil the values
+        firstBox = nil
+        secondBox = nil
+        
     }
     
     
